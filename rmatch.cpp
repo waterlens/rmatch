14550f22c667fb95e082160ee5443e9f7b3d9ca4
Waterlens <82268070+waterlens@users.noreply.github.com>
2021-09-28 00:01:31 +0800

    initial x86_64 support

--- rmatch.cpp
+++ rmatch.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <fmt/core.h>
 #include <fmt/format.h>
 #include <list>
@@ -9,6 +10,7 @@
 #include <string>
 #include <string_view>
 #include <xbyak.h>
+#include <xbyak_util.h>
 
 #ifdef XBYAK32
 #error "this matcher is for only 64-bit mode"
@@ -25,27 +27,135 @@ using fmt::print;
   factor  ::= '.' | char | escaped_char | '(' expr ')' ;
 */
 
+/*
+  entry:
+    ; allocate stack frame
+    mov   a0, st.p[0]
+  reach_string_end:
+    movzx eax, byte ptr [a0]
+    jnz   run
+  match_fail:
+    xor   eax, eax
+  match_return:
+    ; clear stack and return
+  backtrack:
+    cmp   rsp, rbp
+    je    match_fail
+    pop   a0
+    pop   r9
+    jmp   r9
+
+*/
+
 enum Instruction {
+  /*
+    push  L2
+    push  a0
+    jmp   L1
+   */
   SPLIT,
-  MATCH,
+  /*
+    cmp   byte ptr [a0], `char`
+    jne   thread_fail
+    inc   a0
+   */
+  SINGLE,
+  /*
+    movzx eax, byte ptr [a0]
+    jz    thread_fail
+    inc   a0
+   */
+  ANY,
+  /*
+    jmp L1
+   */
   JUMP,
-  BEGIN,
-  END,
+  /*
+    mov   eax, 1
+    jmp   match_return
+   */
   ACCEPT,
   LABEL,
-  ANY,
-  SINGLE,
 };
+using match_prototype = bool (*)(const char *);
 
 struct Compiler : Xbyak::CodeGenerator {
   Compiler() : Xbyak::CodeGenerator(4096, Xbyak::AutoGrow) {}
+  void compile(const list<uint32_t> &instructions) {
+    vector<uint32_t> linear_inst(instructions.cbegin(), instructions.cend());
+    Xbyak::util::StackFrame sf(this, 1, 0, 0, false);
+    const auto &a0 = sf.p[0];
+    L("reach_string_end");
+    {
+      movzx(eax, byte[a0]);
+      push(rbp);
+      mov(rbp, rsp);
+      jmp("run");
+    }
+    L("match_fail");
+    { xor_(eax, eax); }
+    L("match_return");
+    {
+      mov(rsp, rbp);
+      pop(rbp);
+      sf.close();
+    }
+    L("thread_fail");
+    {
+      cmp(rsp, rbp);
+      je("match_fail");
+      pop(a0);
+      pop(r9);
+      jmp(r9);
+    }
+    L("run");
+    Xbyak::Label l;
+    string s;
+    for (size_t i = 0; i < linear_inst.size();) {
+      switch (linear_inst[i]) {
+      case SPLIT:
+        s = format("L{}", linear_inst.at(i + 1));
+        mov(rax, s.c_str());
+        sub(rsp, 16);
+        mov(ptr[rsp], a0);
+        mov(ptr[rsp + 8], rax);
+        jmp(format("L{}", linear_inst.at(i + 2)));
+        i += 3;
+        break;
+      case SINGLE:
+        cmp(byte[a0], linear_inst.at(i + 1));
+        jne("thread_fail");
+        inc(a0);
+        i += 2;
+        break;
+      case ANY:
+        movzx(eax, byte[a0]);
+        jnz("thread_fail");
+        inc(a0);
+        i += 1;
+        break;
+      case JUMP:
+        jmp(format("L{}", linear_inst.at(i + 1)));
+        i += 2;
+        break;
+      case ACCEPT:
+        mov(eax, 1);
+        jmp("match_return");
+        i += 1;
+        break;
+      case LABEL:
+        L(format("L{}", linear_inst.at(i + 1)));
+        i += 2;
+        break;
+      }
+    }
+  }
 };
 
 struct Matcher {
   string re;
   string::const_iterator iter;
   uint32_t label_id;
-  list<uint32_t> instructions;
 
   Matcher(const string &re) : re(re), iter(this->re.cbegin()), label_id() {}
 
@@ -57,16 +167,9 @@ struct Matcher {
     switch (l) {
     case 0: {
       list<uint32_t> inst;
-      if (not_end() && *iter == '^') {
-        inst.push_back(BEGIN);
-        next_iter();
-      }
       auto r = parse(1);
-      inst.insert(inst.end(), r.cbegin(), r.cend());
-      if (not_end() && *iter == '$') {
-        inst.push_back(END);
-        next_iter();
-      }
+      inst.insert(inst.begin(), r.cbegin(), r.cend());
+      inst.push_back(ACCEPT);
       return inst;
     }
     case 1: {
@@ -207,9 +310,12 @@ struct Matcher {
 };
 
 int main() {
-  string re("(a|b)*abb+c?");
-  Matcher match(re);
-  for (auto &&i : match.parse(0)) {
-    print("{}\n", i);
-  }
+  string re("(a|b)");
+  Matcher matcher(re);
+  Compiler compiler;
+  compiler.compile(matcher.parse(0));
+  compiler.readyRE();
+  auto f = compiler.getCode<match_prototype>();
+  fwrite(compiler.getCode(), 1, compiler.getSize(), stdout);
+  return f("a");
 }
---
+++ test.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+build/rmatch > build/dynbin
+objdump -M x86-64,intel-mnemonic -D -b binary -m i386 build/dynbin
